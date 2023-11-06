121f6c0476783c2c3d21aca415fb7615ece0799c
ZeroLu <iamzerolu@gmail.com>
2023-11-06 15:17:53 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = 'e3658f66-9545-423d-ab44-81f93315fed4'
+export const API_KEY = 'app-Lhf18wFK2UPyLPS1aVWvK1XI'
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
